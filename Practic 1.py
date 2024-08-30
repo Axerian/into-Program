9d81431cb60ef6215cd58d5ac401e6cfcab6cf65
Axerian <axelgu2006@gmail.com>
2024-08-30 07:06:24 -0500

    Add files via upload

---
+++ Practic 1.py
@@ -0,0 +1,12 @@
+def say_hi(name ="a todos"):
+    print("Hola Humano (name)")
+def multiplicar (num1, num2):
+    return num1*num2
+def multi_with_print (num1, num2):
+    print(num1 * num2)
+
+result = multiplicar(2,4)
+resultim=  multi_with_print (2,10)
+print(result)
+print(resultim)
+say_hi()
